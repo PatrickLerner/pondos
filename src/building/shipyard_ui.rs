dc7d0b9fef469f22e92c402ba19666ffff31322d
Patrick Lerner <ptlerner@gmail.com>
2022-10-11 23:58:33 +0200

    improve shipyard ui

--- src/building/shipyard_ui.rs
+++ src/building/shipyard_ui.rs
@@ -11,6 +11,24 @@ use bevy_egui::{
     EguiContext,
 };
 
+pub struct ShipTextures {
+    #[allow(dead_code)]
+    references: Vec<Handle<Image>>,
+    small: egui::TextureId,
+    medium: egui::TextureId,
+    large: egui::TextureId,
+}
+
+impl ShipTextures {
+    fn transport_type_image(&self, transport_type: TransportType) -> egui::TextureId {
+        match transport_type {
+            TransportType::SmallShip => self.small,
+            TransportType::MediumShip => self.medium,
+            TransportType::LargeShip => self.large,
+        }
+    }
+}
+
 pub fn shipyard_ui(
     mut egui_context: ResMut<EguiContext>,
     selected_building: Res<Option<SelectedBuilding>>,
@@ -18,10 +36,28 @@ pub fn shipyard_ui(
     mut game_state: ResMut<State<GameState>>,
     windows: Res<Windows>,
     mut player: ResMut<Player>,
+    asset_server: Res<AssetServer>,
+    mut ship_textures: Local<Option<ShipTextures>>,
 ) {
+    if ship_textures.is_none() {
+        let small = asset_server.load("images/ship_small.png");
+        let medium = asset_server.load("images/ship_medium.png");
+        let large = asset_server.load("images/ship_large.png");
+
+        let textures = ShipTextures {
+            small: egui_context.add_image(small.clone_weak()),
+            medium: egui_context.add_image(medium.clone_weak()),
+            large: egui_context.add_image(large.clone_weak()),
+            references: vec![small, medium, large],
+        };
+        *ship_textures = Some(textures);
+    }
+
     if let Some(entity) = selected_building.as_ref() {
         if let Ok(mut shipyard) = shipyards.get_mut(entity.0) {
             let mut open = true;
+
+            let ship_textures = ship_textures.as_ref().unwrap();
 
             create_window(
                 egui_context.ctx_mut(),
@@ -38,49 +74,84 @@ pub fn shipyard_ui(
                         }
                     });
 
-                    if let Some(construction) = shipyard.construction {
-                        if shipyard.construction_time == 0 {
-                            ui.label(format!("{} ready for pickup", construction));
+                    if let Some(transport_type) = shipyard.construction {
+                        ui.with_layout(
+                            egui::Layout::from_main_dir_and_cross_align(
+                                egui::Direction::TopDown,
+                                egui::Align::Center,
+                            ),
+                            |ui| {
+                                ui.add(egui::widgets::Image::new(
+                                    ship_textures.transport_type_image(transport_type),
+                                    [200.0, 250.0],
+                                ));
+                                ui.heading(format!("{}", transport_type));
+                                ui.add_space(5.);
 
-                            if large_button(ui, 100., "Pickup").clicked() {
-                                shipyard.construction = None;
-                                player.convoy.push(construction);
-                            }
-                        } else {
-                            ui.label(format!(
-                                "Constructing {} for {} seasons",
-                                construction, shipyard.construction_time
-                            ));
-                        }
+                                if shipyard.construction_time == 0 {
+                                    ui.label("Ready for pickup");
+                                    ui.add_space(10.);
+
+                                    if large_button(ui, 200., "Add to convoy").clicked() {
+                                        shipyard.construction = None;
+                                        player.convoy.push(transport_type);
+                                    }
+                                } else if shipyard.construction_time == 1 {
+                                    ui.label("In construction for one more seasons");
+                                } else {
+                                    ui.label(format!(
+                                        "In construction for {} more seasons",
+                                        shipyard.construction_time
+                                    ));
+                                }
+                            },
+                        );
                     } else {
-                        for transport_type in vec![
-                            TransportType::SmallShip,
-                            TransportType::MediumShip,
-                            TransportType::LargeShip,
-                        ]
-                        .into_iter()
-                        {
-                            let enabled = player.silver >= transport_type.price();
+                        ui.columns(3, |columns| {
+                            for (index, transport_type) in vec![
+                                TransportType::SmallShip,
+                                TransportType::MediumShip,
+                                TransportType::LargeShip,
+                            ]
+                            .into_iter()
+                            .enumerate()
+                            {
+                                let enabled = player.silver >= transport_type.price();
 
-                            let button = ui.add_sized(
-                                [300., 30.],
-                                egui::Button::new(
-                                    RichText::new(format!(
-                                        "Construct a {} ({} {})",
-                                        transport_type,
-                                        transport_type.price(),
-                                        COIN_NAME
-                                    ))
-                                    .color(enabled_color(enabled)),
-                                ),
-                            );
+                                columns[index].with_layout(
+                                    egui::Layout::from_main_dir_and_cross_align(
+                                        egui::Direction::TopDown,
+                                        egui::Align::Center,
+                                    ),
+                                    |ui| {
+                                        ui.add(egui::widgets::Image::new(
+                                            ship_textures.transport_type_image(transport_type),
+                                            [100.0, 125.0],
+                                        ));
+                                        ui.heading(format!("{}", transport_type));
+                                        ui.label(format!(
+                                            "{} {}",
+                                            transport_type.price(),
+                                            COIN_NAME
+                                        ));
+                                    },
+                                );
 
-                            if button.clicked() && enabled {
-                                player.silver -= transport_type.price();
-                                shipyard.construction = Some(transport_type);
-                                shipyard.construction_time = 5;
+                                columns[index].add_space(5.);
+
+                                let button = columns[index].add_sized(
+                                    [100., 30.],
+                                    egui::Button::new(
+                                        RichText::new("Construct").color(enabled_color(enabled)),
+                                    ),
+                                );
+                                if button.clicked() && enabled {
+                                    player.silver -= transport_type.price();
+                                    shipyard.construction = Some(transport_type);
+                                    shipyard.construction_time = transport_type.construction_time();
+                                }
                             }
-                        }
+                        });
                     }
                 },
             );
